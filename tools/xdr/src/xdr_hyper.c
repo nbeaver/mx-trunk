4983efd96277d7481d728973b723601b7f80fb7a
lavender <lavender@42547f41-83fc-0310-9aef-9668e019ab9e>
2006-02-11 02:28:54 +0000

    Add xdr_hyper.c and xdr_hyper.h that were mistakenly left out of the last commit.
    git-svn-id: http://svn.csrri.iit.edu/mx/trunk@264 42547f41-83fc-0310-9aef-9668e019ab9e

---
+++ tools/xdr/src/xdr_hyper.c
@@ -0,0 +1,117 @@
+/*
+ * Name:    xdr_hyper.c
+ *
+ * Purpose: This file supplies the 64-bit xdr_hyper() and xdr_u_hyper()
+ *          functions for those platforms whose XDR implemtations do not
+ *          already include them such as MacOS X.
+ *
+ * Author:  William Lavender
+ *
+ *------------------------------------------------------------------------
+ *
+ * Copyright 2006 Illinois Institute of Technology
+ *
+ * See the file "LICENSE" for information on usage and redistribution
+ * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+ *
+ */
+
+#include <stdio.h>
+#include <limits.h>
+#include <rpc/types.h>
+#include <rpc/xdr.h>
+
+#include "mx_util.h"
+#if 0
+#include "mx_stdint.h"
+#include "mx_bit.h"
+#include "mx_socket.h"
+#endif
+
+#include "xdr_hyper.h"
+
+MX_EXPORT bool_t
+mx_xdr_hyper( XDR *xdrs, quad_t *quad_ptr )
+{
+	u_quad_t *u_quad_ptr;
+	u_long u_long_array[2];
+	bool_t status;
+
+	switch( xdrs->x_op) {
+	case XDR_ENCODE:
+		u_quad_ptr = (u_quad_t *) quad_ptr;
+
+		u_long_array[0] = (u_long) ((*u_quad_ptr >> 32) & 0xffffffff);
+		u_long_array[1] = (u_long) ((*u_quad_ptr) & 0xffffffff);
+
+		status = XDR_PUTLONG( xdrs, (long *) &u_long_array[0] );
+
+		if ( status == FALSE )
+			return FALSE;
+
+		status = XDR_PUTLONG( xdrs, (long *) &u_long_array[1] );
+
+		return status;
+	case XDR_DECODE:
+		status = XDR_GETLONG( xdrs, (long *) &u_long_array[0] );
+
+		if ( status == FALSE )
+			return FALSE;
+
+		status = XDR_GETLONG( xdrs, (long *) &u_long_array[1] );
+
+		if ( status == FALSE )
+			return FALSE;
+
+		*quad_ptr = (quad_t)( ( ((u_quad_t) u_long_array[0]) << 32 )
+				      | ((u_quad_t) u_long_array[1]) );
+
+		return TRUE;
+	case XDR_FREE:
+		return TRUE;
+	}
+
+	return FALSE;
+}
+
+MX_EXPORT bool_t
+mx_xdr_u_hyper( XDR *xdrs, u_quad_t *u_quad_ptr )
+{
+	u_long u_long_array[2];
+	bool_t status;
+
+	switch( xdrs->x_op) {
+	case XDR_ENCODE:
+		u_long_array[0] = (u_long) ((*u_quad_ptr >> 32) & 0xffffffff);
+		u_long_array[1] = (u_long) ((*u_quad_ptr) & 0xffffffff);
+
+		status = XDR_PUTLONG( xdrs, (long *) &u_long_array[0] );
+
+		if ( status == FALSE )
+			return FALSE;
+
+		status = XDR_PUTLONG( xdrs, (long *) &u_long_array[1] );
+
+		return status;
+	case XDR_DECODE:
+		status = XDR_GETLONG( xdrs, (long *) &u_long_array[0] );
+
+		if ( status == FALSE )
+			return FALSE;
+
+		status = XDR_GETLONG( xdrs, (long *) &u_long_array[1] );
+
+		if ( status == FALSE )
+			return FALSE;
+
+		*u_quad_ptr = (u_quad_t)( ( ((u_quad_t) u_long_array[0]) << 32 )
+				          | ((u_quad_t) u_long_array[1]) );
+
+		return TRUE;
+	case XDR_FREE:
+		return TRUE;
+	}
+
+	return FALSE;
+}
+
---
+++ tools/xdr/src/xdr_hyper.h
@@ -0,0 +1,30 @@
+/*
+ * Name:    xdr_hyper.h
+ *
+ * Purpose: This header file supplies the 64-bit xdr_hyper() and xdr_u_hyper()
+ *          functions for those platforms whose XDR implementation do not 
+ *          already include them such as MacOS X.
+ *
+ * Author:  William Lavender
+ *
+ *------------------------------------------------------------------------
+ *
+ * Copyright 2006 Illinois Institute of Technology
+ *
+ * See the file "LICENSE" for information on usage and redistribution
+ * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+ *
+ */
+
+#ifndef __XDR_HYPER_H__
+#define __XDR_HYPER_H__
+
+#define xdr_hyper	mx_xdr_hyper
+#define xdr_u_hyper	mx_xdr_u_hyper
+
+MX_API bool_t mx_xdr_hyper( XDR *xdrs, quad_t *quad_ptr );
+
+MX_API bool_t mx_xdr_u_hyper( XDR *xdrs, u_quad_t *u_quad_ptr );
+
+#endif /* __XDR_HYPER_H__ */
+
