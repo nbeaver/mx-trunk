4b7799ad744c2e249f989fc42ad727eeee4bc5e5
lavender <lavender@42547f41-83fc-0310-9aef-9668e019ab9e>
2006-03-24 21:00:47 +0000

    MacOS X updates
    git-svn-id: http://svn.csrri.iit.edu/mx/trunk@326 42547f41-83fc-0310-9aef-9668e019ab9e

--- libMx/mx_cpu_architecture.c
+++ libMx/mx_cpu_architecture.c
@@ -21,7 +21,7 @@
 
 #include "mx_util.h"
 
-#if defined( OS_UNIX )
+#if defined(OS_UNIX)
 
 #include <sys/utsname.h>
 
@@ -46,6 +46,37 @@ mx_get_cpu_architecture( char *architecture_type,
 			saved_errno, strerror( saved_errno ) );
 	}
 
+#if 1
+	MX_DEBUG(-2,("%s: sysname = '%s'", fname, uname_struct.sysname));
+	MX_DEBUG(-2,("%s: nodename = '%s'", fname, uname_struct.nodename));
+	MX_DEBUG(-2,("%s: release = '%s'", fname, uname_struct.release));
+	MX_DEBUG(-2,("%s: version = '%s'", fname, uname_struct.version));
+	MX_DEBUG(-2,("%s: machine = '%s'", fname, uname_struct.machine));
+#endif
+
+#if defined(OS_MACOSX)
+	if ( architecture_type != NULL ) {
+
+		/* This is the way the Darwin version of uname(1) does it. */
+#  if defined(__ppc__)
+		strlcpy( architecture_type, "powerpc",
+				max_architecture_type_length );
+#  elif defined(__i386__)
+		strlcpy( architecture_type, "i386",
+				max_architecture_type_length );
+#  else
+		strlcpy( architecture_type, "unknown",
+				max_architecture_type_length );
+#  endif
+	}
+
+	if ( architecture_subtype != NULL ) {
+		strlcpy( architecture_subtype, uname_struct.machine,
+				max_architecture_subtype_length );
+	}
+
+#else	/* OS_LINUX */
+
 	if ( architecture_type != NULL ) {
 		strlcpy( architecture_type, uname_struct.machine, 
 				max_architecture_type_length );
@@ -55,6 +86,7 @@ mx_get_cpu_architecture( char *architecture_type,
 		strlcpy( architecture_subtype, "",
 				max_architecture_subtype_length );
 	}
+#endif
 
 	return MX_SUCCESSFUL_RESULT;
 }
