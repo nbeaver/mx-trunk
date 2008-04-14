72d93764231b6c7ab1cd22273126e2320276776a
lavender <lavender@42547f41-83fc-0310-9aef-9668e019ab9e>
2008-04-14 23:21:11 +0000

    The U500 driver can now read the output of MESSAGE FILE commands as long as they are written to a Windows Named Pipe that is read by the 'u500_rs232' driver.
    git-svn-id: http://svn.csrri.iit.edu/mx/trunk@1100 42547f41-83fc-0310-9aef-9668e019ab9e

--- libMx/i_u500_rs232.c
+++ libMx/i_u500_rs232.c
@@ -21,6 +21,10 @@
 
 #if HAVE_U500
 
+#if defined(OS_WIN32)
+#include <windows.h>
+#endif
+
 #include <stdlib.h>
 
 #include "mx_util.h"
@@ -32,7 +36,13 @@
 MX_RECORD_FUNCTION_LIST mxi_u500_rs232_record_function_list = {
 	NULL,
 	mxi_u500_rs232_create_record_structures,
-	mxi_u500_rs232_finish_record_initialization
+	mxi_u500_rs232_finish_record_initialization,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	mxi_u500_rs232_open,
+	mxi_u500_rs232_close
 };
 
 MX_RS232_FUNCTION_LIST mxi_u500_rs232_rs232_function_list = {
@@ -40,8 +50,9 @@ MX_RS232_FUNCTION_LIST mxi_u500_rs232_rs232_function_list = {
 	NULL,
 	NULL,
 	NULL,
-	NULL,
+	mxi_u500_rs232_getline,
 	mxi_u500_rs232_putline,
+	mxi_u500_rs232_num_input_bytes_available
 };
 
 MX_RECORD_FIELD_DEFAULTS mxi_u500_rs232_record_field_defaults[] = {
@@ -154,6 +165,8 @@ mxi_u500_rs232_create_record_structures( MX_RECORD *record )
 	rs232->record = record;
 	u500_rs232->record = record;
 
+	u500_rs232->pipe_handle = INVALID_HANDLE_VALUE;
+
 	return MX_SUCCESSFUL_RESULT;
 }
 
@@ -177,6 +190,186 @@ mxi_u500_rs232_finish_record_initialization( MX_RECORD *record )
 }
 
 MX_EXPORT mx_status_type
+mxi_u500_rs232_open( MX_RECORD *record )
+{
+	static const char fname[] = "mxi_u500_rs232_open()";
+
+	MX_RS232 *rs232;
+	MX_U500_RS232 *u500_rs232;
+	int os_major, os_minor, os_update;
+	mx_status_type mx_status;
+
+	DWORD open_mode, pipe_mode, max_instances;
+	DWORD out_buffer_size, in_buffer_size, default_timeout;
+	DWORD last_error_code;
+	TCHAR message_buffer[100];
+
+	if ( record == (MX_RECORD *) NULL ) {
+		return mx_error( MXE_NULL_ARGUMENT, fname,
+		"The MX_RECORD pointer passed was NULL." );
+	}
+
+	rs232 = record->record_class_struct;
+
+	mx_status = mxi_u500_rs232_get_pointers( rs232,
+						&u500_rs232, NULL, fname );
+	if ( mx_status.code != MXE_SUCCESS )
+		return mx_status;
+
+	u500_rs232->pipe_handle = INVALID_HANDLE_VALUE;
+
+	/* The 'u500_rs232' driver provides the option of reading text strings
+	 * produced using the U500's MESSAGE FILE command by providing a named
+	 * pipe for the U500 to write the messages to.
+	 *
+	 * If the length of the named pipe is zero, then it is assumed that
+	 * we do not want to make use of the MESSAGE FILE named pipe feature.
+	 */
+
+	if ( strlen(u500_rs232->pipe_name) == 0 )
+		return MX_SUCCESSFUL_RESULT;
+
+	mx_status = mx_get_os_version( &os_major, &os_minor, &os_update );
+
+	if ( mx_status.code != MXE_SUCCESS )
+		return mx_status;
+
+	/* If requested, create the named pipe now.  Only one pipe
+	 * with a given name is allowed to exist.
+	 *
+	 * Note: CreateNamedPipe() will fail on Windows 95/98/ME.
+	 */
+
+	open_mode = PIPE_ACCESS_INBOUND;
+	pipe_mode = PIPE_TYPE_BYTE | PIPE_READMODE_BYTE | PIPE_NOWAIT;
+	max_instances = 1;
+	out_buffer_size = 0;
+	in_buffer_size = 1000;
+	default_timeout = 1000;    /* in milliseconds */
+
+	u500_rs232->pipe_handle = CreateNamedPipe( u500_rs232->pipe_name,
+							open_mode,
+							pipe_mode,
+							max_instances,
+							out_buffer_size,
+							in_buffer_size,
+							default_timeout,
+							NULL );
+
+	if ( u500_rs232->pipe_handle == INVALID_HANDLE_VALUE ) {
+		last_error_code = GetLastError();
+
+		mx_win32_error_message( last_error_code,
+			message_buffer, sizeof(message_buffer) );
+
+		return mx_error( MXE_IPC_IO_ERROR, fname,
+		"Unable to create the named pipe '%s' for record '%s'.  "
+		"Win32 error code = %ld, error message = '%s'.",
+			u500_rs232->pipe_name, record->name,
+			last_error_code, message_buffer );
+	}
+
+	return MX_SUCCESSFUL_RESULT;
+}
+
+MX_EXPORT mx_status_type
+mxi_u500_rs232_close( MX_RECORD *record )
+{
+	static const char fname[] = "mxi_u500_rs232_close()";
+
+	MX_RS232 *rs232;
+	MX_U500_RS232 *u500_rs232;
+	mx_status_type mx_status;
+
+	BOOL close_status;
+	DWORD last_error_code;
+	TCHAR message_buffer[100];
+
+	if ( record == (MX_RECORD *) NULL ) {
+		return mx_error( MXE_NULL_ARGUMENT, fname,
+		"The MX_RECORD pointer passed was NULL." );
+	}
+
+	rs232 = record->record_class_struct;
+
+	mx_status = mxi_u500_rs232_get_pointers( rs232,
+						&u500_rs232, NULL, fname );
+	if ( mx_status.code != MXE_SUCCESS )
+		return mx_status;
+
+	if ( u500_rs232->pipe_handle == INVALID_HANDLE_VALUE )
+		return MX_SUCCESSFUL_RESULT;
+
+	close_status = CloseHandle( u500_rs232->pipe_handle );
+
+	u500_rs232->pipe_handle = INVALID_HANDLE_VALUE;
+
+	if ( close_status == 0 ) {
+		last_error_code = GetLastError();
+
+		mx_win32_error_message( last_error_code,
+			message_buffer, sizeof(message_buffer) );
+
+		return mx_error( MXE_IPC_IO_ERROR, fname,
+		"Unable to close named pipe '%s' for record '%s'.  "
+		"Win32 error code = %ld, error message = '%s'.",
+			u500_rs232->pipe_name, record->name,
+			last_error_code, message_buffer );
+	}
+
+	return MX_SUCCESSFUL_RESULT;
+}
+
+MX_EXPORT mx_status_type
+mxi_u500_rs232_getline( MX_RS232 *rs232,
+			char *buffer,
+			size_t max_bytes_to_read,
+			size_t *bytes_read )
+{
+	static const char fname[] = "mxi_u500_rs232_getline()";
+
+	MX_U500_RS232 *u500_rs232;
+	mx_status_type mx_status;
+
+	BOOL read_status;
+	DWORD win32_bytes_read;
+	DWORD last_error_code;
+	TCHAR message_buffer[100];
+
+	mx_status = mxi_u500_rs232_get_pointers( rs232,
+						&u500_rs232, NULL, fname );
+	if ( mx_status.code != MXE_SUCCESS )
+		return mx_status;
+
+	if ( u500_rs232->pipe_handle == INVALID_HANDLE_VALUE ) {
+		return mx_error( MXE_NOT_VALID_FOR_CURRENT_STATE, fname,
+		"The named pipe '%s' is not open for record '%s'.",
+			u500_rs232->pipe_name, rs232->record->name );
+	}
+
+	read_status = ReadFile( u500_rs232->pipe_handle, buffer,
+				max_bytes_to_read, &win32_bytes_read, NULL );
+
+	if ( read_status == 0 ) {
+		last_error_code = GetLastError();
+
+		mx_win32_error_message( last_error_code,
+				message_buffer, sizeof(message_buffer) );
+
+		return mx_error( MXE_IPC_IO_ERROR, fname,
+		"An attempt to read from named pipe '%s' for record '%s' "
+		"failed.  Win32 error code = %ld, error message = '%s'.",
+			last_error_code, message_buffer );
+	}
+
+	if ( bytes_read != (size_t *) NULL ) {
+		*bytes_read = (size_t) win32_bytes_read;
+	}
+
+	return MX_SUCCESSFUL_RESULT;
+}
+
+MX_EXPORT mx_status_type
 mxi_u500_rs232_putline( MX_RS232 *rs232,
 			char *buffer,
 			size_t *bytes_written )
@@ -195,6 +388,63 @@ mxi_u500_rs232_putline( MX_RS232 *rs232,
 	mx_status = mxi_u500_command( u500, u500_rs232->board_number, buffer );
 
 	return mx_status;
+}
+
+MX_EXPORT mx_status_type
+mxi_u500_rs232_num_input_bytes_available( MX_RS232 *rs232 )
+{
+	static const char fname[] =
+		"mxi_u500_rs232_num_input_bytes_available()";
+
+	MX_U500_RS232 *u500_rs232;
+	mx_status_type mx_status;
+
+	BOOL pipe_status;
+	DWORD bytes_read, total_bytes_available;
+	DWORD last_error_code;
+	TCHAR message_buffer[100];
+	TCHAR peek_buffer[1000];
+
+	mx_status = mxi_u500_rs232_get_pointers( rs232,
+						&u500_rs232, NULL, fname );
+	if ( mx_status.code != MXE_SUCCESS )
+		return mx_status;
+
+	/* If the named pipe is not open, then we merely state that
+	 * zero bytes are available.
+	 */
+
+	if ( u500_rs232->pipe_handle == INVALID_HANDLE_VALUE ) {
+		rs232->num_input_bytes_available = 0;
+		return MX_SUCCESSFUL_RESULT;
+	}
+
+	pipe_status = PeekNamedPipe( u500_rs232->pipe_handle,
+			peek_buffer, sizeof(peek_buffer),
+			&bytes_read, &total_bytes_available, NULL );
+
+#if 1
+	MX_DEBUG(-2,("%s: bytes_read = %ld, total_bytes_available = %ld",
+		fname, (long) bytes_read, (long) total_bytes_available));
+#endif
+
+	if ( pipe_status == 0 ) {
+		last_error_code = GetLastError();
+
+		mx_win32_error_message( last_error_code,
+			message_buffer, sizeof(message_buffer) );
+
+		return mx_error( MXE_IPC_IO_ERROR, fname,
+		"An attempt to determine the number of bytes available "
+		"in named pipe '%s' for record '%s' failed.  "
+		"Win32 error code = %ld, error message = '%s'.",
+			u500_rs232->pipe_name, rs232->record->name,
+			last_error_code, message_buffer );
+	}
+
+	rs232->num_input_bytes_available = total_bytes_available;
+
+	return MX_SUCCESSFUL_RESULT;
 }
 
 #endif /* HAVE_U500 */
--- libMx/i_u500_rs232.h
+++ libMx/i_u500_rs232.h
@@ -23,16 +23,30 @@ typedef struct {
 
 	MX_RECORD *u500_record;
 	long board_number;
+	char pipe_name[MXU_FILENAME_LENGTH+1];
+
+#if defined(OS_WIN32)
+	HANDLE pipe_handle;
+#endif
 } MX_U500_RS232;
 
 MX_API mx_status_type mxi_u500_rs232_create_record_structures(
 						MX_RECORD *record );
 MX_API mx_status_type mxi_u500_rs232_finish_record_initialization(
 						MX_RECORD *record );
+MX_API mx_status_type mxi_u500_rs232_open( MX_RECORD *record );
+MX_API mx_status_type mxi_u500_rs232_close( MX_RECORD *record );
+
+MX_API mx_status_type mxi_u500_rs232_getline( MX_RS232 *rs232,
+						char *buffer,
+						size_t max_bytes_to_read,
+						size_t *bytes_written );
 
 MX_API mx_status_type mxi_u500_rs232_putline( MX_RS232 *rs232,
 						char *buffer,
 						size_t *bytes_written );
+
+MX_API mx_status_type mxi_u500_rs232_num_input_bytes_available(MX_RS232 *rs232);
 
 extern MX_RECORD_FUNCTION_LIST mxi_u500_rs232_record_function_list;
 extern MX_RS232_FUNCTION_LIST mxi_u500_rs232_rs232_function_list;
@@ -47,7 +61,11 @@ extern MX_RECORD_FIELD_DEFAULTS *mxi_u500_rs232_rfield_def_ptr;
   \
   {-1, -1, "board_number", MXFT_LONG, NULL, 0, {0}, \
 	MXF_REC_TYPE_STRUCT, offsetof(MX_U500_RS232, board_number), \
-	{0}, NULL, (MXFF_IN_DESCRIPTION | MXFF_IN_SUMMARY)}
+	{0}, NULL, (MXFF_IN_DESCRIPTION | MXFF_IN_SUMMARY)}, \
+  \
+  {-1, -1, "pipe_name", MXFT_STRING, NULL, 1, {MXU_FILENAME_LENGTH}, \
+	MXF_REC_TYPE_STRUCT, offsetof(MX_U500_RS232, pipe_name), \
+	{sizeof(char)}, NULL, (MXFF_IN_DESCRIPTION | MXFF_IN_SUMMARY)}
 
 #endif /* __I_U500_RS232_H__ */
 
