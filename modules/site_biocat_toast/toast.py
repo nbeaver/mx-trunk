f6797a211ddaeffe152a6fc1ce7ba5006fed82fa
lavender <lavender@42547f41-83fc-0310-9aef-9668e019ab9e>
2013-12-18 00:49:29 +0000

    Updates to the BioCAT toast program.  There may be some recoverable exceptions when you abort toast, but the MX server can cope.
    git-svn-id: http://svn.csrri.iit.edu/mx/trunk@2761 42547f41-83fc-0310-9aef-9668e019ab9e

--- modules/site_biocat_toast/toast.py
+++ modules/site_biocat_toast/toast.py
@@ -1,56 +1,78 @@
 #! /usr/bin/env mpscript
 
-import sys
-
-motor = None
-mount = 0.0
-
-def user_interrupt():
-	print "User has interrupted toast."
-	print "Exiting and moving toaster to mid_pos=%f" % ( mount )
-
-	motor.move_absolute( mount )
-
-	motor.wait_for_motor_stop( 0 )
-
-	print "Move complete."
-
-	sys.exit(0)
+import sys, time
 
 def main( database, argv ):
-	global motor
-	global mount
 
-	start      = -0.5
-	end        =  0.5
-	motor_name = "m1"
+	if ( len(argv) != 3 ):
+		print "Usage: toast 'motor_name' 'start' 'end'"
+		sys.exit(0)
+
+	motor_name = argv[0]
+	start      = float( argv[1] )
+	end        = float( argv[2] )
 
 	motor = database.get_record( motor_name )
 
 	mount = start + ( end - start ) / 2.0
 
-	Mp.set_user_interrupt_function( user_interrupt )
-
 	print "Running toaster.  Press any key to stop."
 
-	while 1:
-		print "Running to %f" % ( start )
+	try:
+		while 1:
+			print "Running to %f" % ( start )
+	
+			motor.move_absolute( start )
+	
+			motor.wait_for_motor_stop( 0 )
+	
+			current_position = motor.get_position()
+	
+			print "Arrived at %f" % ( current_position )
+	
+			time.sleep(1)
+	
+			print "Running to %f" % ( end )
+	
+			motor.move_absolute( end )
+	
+			motor.wait_for_motor_stop( 0 )
+	
+			current_position = motor.get_position()
+	
+			print "Arrived at %f" % ( current_position )
+	
+			time.sleep(1)
+	
+	except Mp.Interrupted_Error:
 
-		motor.move_absolute( start )
+		print "User has interrupted toast."
+		print "Exiting and moving toaster to mid_pos=%f" % ( mount )
 
+		# FIXME:
+		# The script may die with an exception due to a
+		# 'Failed to read a character ...' error on the
+		# first attempt to send a command to the Compumotor
+		# after a user interrupt.  For now the workaround
+		# is to run a "safe" command like get_position()
+		# and throw away the error if it occurs.
+		#
+		# This is probably really an error in the MX 
+		# Compumotor drivers.
+		try:
+			position = motor.get_position()
+		except:
+			pass
+
+		motor.soft_abort()
+	
 		motor.wait_for_motor_stop( 0 )
-
-		current_position = motor.get_position()
-
-		print "Arrived at %f" % ( current_position )
-
-		print "Running to %f" % ( end )
-
-		motor.move_absolute( end )
-
+	
+		motor.move_absolute( mount )
+	
 		motor.wait_for_motor_stop( 0 )
-
-		current_position = motor.get_position()
-
-		print "Arrived at %f" % ( current_position )
+	
+		print "Move complete."
+	
+		sys.exit(0)
 
