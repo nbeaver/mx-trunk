7f347f577acda6a1b5945d1dbf1c1a9d1b432c34
lavender <lavender@42547f41-83fc-0310-9aef-9668e019ab9e>
2014-05-07 23:13:28 +0000

    Updates to the condition variable code.
    git-svn-id: http://svn.csrri.iit.edu/mx/trunk@2844 42547f41-83fc-0310-9aef-9668e019ab9e

--- libMx/mx_condition_variable.c
+++ libMx/mx_condition_variable.c
@@ -27,7 +27,59 @@
 
 #if defined(OS_WIN32)
 
-#elif defined(OS_UNIX)
+/************************ Posix pthreads ***********************/
+
+#elif defined(OS_LINUX)
+
+#include <pthread.h>
+
+MX_EXPORT mx_status_type
+mx_condition_variable_create( MX_CONDITION_VARIABLE **cv )
+{
+	static const char fname[] = "mx_condition_variable_create()";
+
+	pthread_cond_t *pthread_cv = NULL;
+	int pthread_status;
+
+	if ( cv == (MX_CONDITION_VARIABLE **) NULL ) {
+		return mx_error( MXE_NULL_ARGUMENT, fname,
+		"The MX_CONDITION_VARIABLE pointer passed was NULL." );
+	}
+
+	*cv = malloc( sizeof(MX_CONDITION_VARIABLE) );
+
+	if ( (*cv) == (MX_CONDITION_VARIABLE *) NULL ) {
+		return mx_error( MXE_OUT_OF_MEMORY, fname,
+  "Ran out of memory trying to allocate an MX_CONDITION_VARIABLE structure." );
+	}
+
+	pthread_cv = malloc( sizeof(pthread_cond_t) );
+
+	if ( pthread_cv == (pthread_cond_t *) NULL ) {
+		mx_free( posix_cv_struct );
+		mx_free( *cv );
+		return mx_error( MXE_OUT_OF_MEMORY, fname,
+	    "Ran out of memory trying to allocate a pthread_cond_t struct." );
+	}
+
+	pthread_status = pthread_cond_init( pthread_cv, NULL );
+
+	if ( pthread_status != 0 ) {
+		mx_free( pthread_cv );
+		mx_free( posix_cv_struct );
+		mx_free( *cv );
+		return mx_error( MXE_OPERATING_SYSTEM_ERROR, fname,
+		"The attempt to initialize the condition variable at %p failed."
+		"  Error code = %d, error message = '%s'",
+		pthread_cv, pthread_status, strerror(pthread_status) );
+	}
+
+	posix_cv_struct->pthread_cv = pthread_cv;
+
+	(*cv)->cv_private = posix_cv_struct;
+
+	return MX_SUCCESSFUL_RESULT;
+}
 
 #else
 
