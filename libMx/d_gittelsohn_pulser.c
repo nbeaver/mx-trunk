f0cee5955eb9a1f5c4a557c03664861a0438b480
lavender <lavender@42547f41-83fc-0310-9aef-9668e019ab9e>
2015-04-20 18:15:14 +0000

    Renamed 'gittelson_pulser' to 'gittelsohn_pulser' (finished rename).
    git-svn-id: http://svn.csrri.iit.edu/mx/trunk@3040 42547f41-83fc-0310-9aef-9668e019ab9e

--- libMx/d_gittelsohn_pulser.c
+++ libMx/d_gittelsohn_pulser.c
@@ -1,7 +1,7 @@
 /*
- * Name:    d_gittelson_pulser.c
+ * Name:    d_gittelsohn_pulser.c
  *
- * Purpose: MX pulse generator driver for Mark Gittelson's Arduino-based
+ * Purpose: MX pulse generator driver for Mark Gittelsohn's Arduino-based
  *          pulse generator.
  *
  * Author:  William Lavender
@@ -15,7 +15,7 @@
  *
  */
 
-#define MXD_GITTELSON_PULSER_DEBUG	FALSE
+#define MXD_GITTELSOHN_PULSER_DEBUG	FALSE
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -25,52 +25,52 @@
 #include "mx_hrt.h"
 #include "mx_rs232.h"
 #include "mx_pulse_generator.h"
-#include "d_gittelson_pulser.h"
+#include "d_gittelsohn_pulser.h"
 
 /* Initialize the pulse generator driver jump table. */
 
-MX_RECORD_FUNCTION_LIST mxd_gittelson_pulser_record_function_list = {
+MX_RECORD_FUNCTION_LIST mxd_gittelsohn_pulser_record_function_list = {
 	NULL,
-	mxd_gittelson_pulser_create_record_structures,
+	mxd_gittelsohn_pulser_create_record_structures,
 	NULL,
 	NULL,
 	NULL,
-	mxd_gittelson_pulser_open
+	mxd_gittelsohn_pulser_open
 };
 
-MX_PULSE_GENERATOR_FUNCTION_LIST mxd_gittelson_pulser_pulser_function_list = {
-	mxd_gittelson_pulser_is_busy,
-	mxd_gittelson_pulser_start,
-	mxd_gittelson_pulser_stop,
-	mxd_gittelson_pulser_get_parameter,
-	mxd_gittelson_pulser_set_parameter
+MX_PULSE_GENERATOR_FUNCTION_LIST mxd_gittelsohn_pulser_pulser_function_list = {
+	mxd_gittelsohn_pulser_is_busy,
+	mxd_gittelsohn_pulser_start,
+	mxd_gittelsohn_pulser_stop,
+	mxd_gittelsohn_pulser_get_parameter,
+	mxd_gittelsohn_pulser_set_parameter
 };
 
 /* MX digital output pulser data structures. */
 
-MX_RECORD_FIELD_DEFAULTS mxd_gittelson_pulser_record_field_defaults[] = {
+MX_RECORD_FIELD_DEFAULTS mxd_gittelsohn_pulser_record_field_defaults[] = {
 	MX_RECORD_STANDARD_FIELDS,
 	MX_PULSE_GENERATOR_STANDARD_FIELDS,
-	MXD_GITTELSON_PULSER_STANDARD_FIELDS
+	MXD_GITTELSOHN_PULSER_STANDARD_FIELDS
 };
 
-long mxd_gittelson_pulser_num_record_fields
-		= sizeof( mxd_gittelson_pulser_record_field_defaults )
-		  / sizeof( mxd_gittelson_pulser_record_field_defaults[0] );
+long mxd_gittelsohn_pulser_num_record_fields
+		= sizeof( mxd_gittelsohn_pulser_record_field_defaults )
+		  / sizeof( mxd_gittelsohn_pulser_record_field_defaults[0] );
 
-MX_RECORD_FIELD_DEFAULTS *mxd_gittelson_pulser_rfield_def_ptr
-			= &mxd_gittelson_pulser_record_field_defaults[0];
+MX_RECORD_FIELD_DEFAULTS *mxd_gittelsohn_pulser_rfield_def_ptr
+			= &mxd_gittelsohn_pulser_record_field_defaults[0];
 
 /*=======================================================================*/
 
 static mx_status_type
-mxd_gittelson_pulser_get_pointers( MX_PULSE_GENERATOR *pulser,
-			MX_GITTELSON_PULSER **gittelson_pulser,
+mxd_gittelsohn_pulser_get_pointers( MX_PULSE_GENERATOR *pulser,
+			MX_GITTELSOHN_PULSER **gittelsohn_pulser,
 			const char *calling_fname )
 {
-	static const char fname[] = "mxd_gittelson_pulser_get_pointers()";
+	static const char fname[] = "mxd_gittelsohn_pulser_get_pointers()";
 
-	MX_GITTELSON_PULSER *gittelson_pulser_ptr;
+	MX_GITTELSOHN_PULSER *gittelsohn_pulser_ptr;
 
 	if ( pulser == (MX_PULSE_GENERATOR *) NULL ) {
 		return mx_error( MXE_NULL_ARGUMENT, fname,
@@ -84,18 +84,18 @@ mxd_gittelson_pulser_get_pointers( MX_PULSE_GENERATOR *pulser,
 			calling_fname );
 	}
 
-	gittelson_pulser_ptr = (MX_GITTELSON_PULSER *)
+	gittelsohn_pulser_ptr = (MX_GITTELSOHN_PULSER *)
 					pulser->record->record_type_struct;
 
-	if ( gittelson_pulser_ptr == (MX_GITTELSON_PULSER *) NULL ) {
+	if ( gittelsohn_pulser_ptr == (MX_GITTELSOHN_PULSER *) NULL ) {
 			return mx_error( MXE_CORRUPT_DATA_STRUCTURE, fname,
-			"The MX_GITTELSON_PULSER pointer for pulse generator "
+			"The MX_GITTELSOHN_PULSER pointer for pulse generator "
 			"record '%s' passed by '%s' is NULL",
 				pulser->record->name, calling_fname );
 	}
 
-	if ( gittelson_pulser != (MX_GITTELSON_PULSER **) NULL ) {
-		*gittelson_pulser = gittelson_pulser_ptr;
+	if ( gittelsohn_pulser != (MX_GITTELSOHN_PULSER **) NULL ) {
+		*gittelsohn_pulser = gittelsohn_pulser_ptr;
 	}
 
 	return MX_SUCCESSFUL_RESULT;
@@ -103,20 +103,20 @@ mxd_gittelson_pulser_get_pointers( MX_PULSE_GENERATOR *pulser,
 
 #if 0
 static mx_status_type
-mxd_gittelson_pulser_command()
+mxd_gittelsohn_pulser_command()
 { return MX_SUCCESSFUL_RESULT; }
 #endif
 
 /*=======================================================================*/
 
 MX_EXPORT mx_status_type
-mxd_gittelson_pulser_create_record_structures( MX_RECORD *record )
+mxd_gittelsohn_pulser_create_record_structures( MX_RECORD *record )
 {
 	static const char fname[] =
-			"mxd_gittelson_pulser_create_record_structures()";
+			"mxd_gittelsohn_pulser_create_record_structures()";
 
 	MX_PULSE_GENERATOR *pulser;
-	MX_GITTELSON_PULSER *gittelson_pulser;
+	MX_GITTELSOHN_PULSER *gittelsohn_pulser;
 
 	/* Allocate memory for the necessary structures. */
 
@@ -127,34 +127,34 @@ mxd_gittelson_pulser_create_record_structures( MX_RECORD *record )
 		"Can't allocate memory for MX_PULSE_GENERATOR structure." );
 	}
 
-	gittelson_pulser = (MX_GITTELSON_PULSER *)
-				malloc( sizeof(MX_GITTELSON_PULSER) );
+	gittelsohn_pulser = (MX_GITTELSOHN_PULSER *)
+				malloc( sizeof(MX_GITTELSOHN_PULSER) );
 
-	if ( gittelson_pulser == NULL ) {
+	if ( gittelsohn_pulser == NULL ) {
 		return mx_error( MXE_OUT_OF_MEMORY, fname,
-		"Can't allocate memory for MX_GITTELSON_PULSER structure." );
+		"Can't allocate memory for MX_GITTELSOHN_PULSER structure." );
 	}
 
 	/* Now set up the necessary pointers. */
 
 	record->record_class_struct = pulser;
-	record->record_type_struct = gittelson_pulser;
+	record->record_type_struct = gittelsohn_pulser;
 	record->class_specific_function_list
-			= &mxd_gittelson_pulser_pulser_function_list;
+			= &mxd_gittelsohn_pulser_pulser_function_list;
 
 	pulser->record = record;
-	gittelson_pulser->record = record;
+	gittelsohn_pulser->record = record;
 
 	return MX_SUCCESSFUL_RESULT;
 }
 
 MX_EXPORT mx_status_type
-mxd_gittelson_pulser_open( MX_RECORD *record )
+mxd_gittelsohn_pulser_open( MX_RECORD *record )
 {
-	static const char fname[] = "mxd_gittelson_pulser_open()";
+	static const char fname[] = "mxd_gittelsohn_pulser_open()";
 
 	MX_PULSE_GENERATOR *pulser;
-	MX_GITTELSON_PULSER *gittelson_pulser = NULL;
+	MX_GITTELSOHN_PULSER *gittelsohn_pulser = NULL;
 	mx_status_type mx_status;
 
 	if ( record == (MX_RECORD *) NULL ) {
@@ -164,20 +164,20 @@ mxd_gittelson_pulser_open( MX_RECORD *record )
 
 	pulser = (MX_PULSE_GENERATOR *) record->record_class_struct;
 
-	mx_status = mxd_gittelson_pulser_get_pointers( pulser,
-						&gittelson_pulser, fname );
+	mx_status = mxd_gittelsohn_pulser_get_pointers( pulser,
+						&gittelsohn_pulser, fname );
 
 	if ( mx_status.code != MXE_SUCCESS )
 		return mx_status;
 
 	mx_status = mx_rs232_discard_unwritten_output(
-					gittelson_pulser->rs232_record, 0 );
+					gittelsohn_pulser->rs232_record, 0 );
 
 	if ( mx_status.code != MXE_SUCCESS )
 		return mx_status;
 
 	mx_status = mx_rs232_discard_unread_input(
-					gittelson_pulser->rs232_record, 0 );
+					gittelsohn_pulser->rs232_record, 0 );
 
 	if ( mx_status.code != MXE_SUCCESS )
 		return mx_status;
@@ -186,20 +186,20 @@ mxd_gittelson_pulser_open( MX_RECORD *record )
 }
 
 MX_EXPORT mx_status_type
-mxd_gittelson_pulser_is_busy( MX_PULSE_GENERATOR *pulser )
+mxd_gittelsohn_pulser_is_busy( MX_PULSE_GENERATOR *pulser )
 {
-	static const char fname[] = "mxd_gittelson_pulser_is_busy()";
+	static const char fname[] = "mxd_gittelsohn_pulser_is_busy()";
 
-	MX_GITTELSON_PULSER *gittelson_pulser = NULL;
+	MX_GITTELSOHN_PULSER *gittelsohn_pulser = NULL;
 	mx_status_type mx_status;
 
-	mx_status = mxd_gittelson_pulser_get_pointers( pulser,
-						&gittelson_pulser, fname );
+	mx_status = mxd_gittelsohn_pulser_get_pointers( pulser,
+						&gittelsohn_pulser, fname );
 
 	if ( mx_status.code != MXE_SUCCESS )
 		return mx_status;
 
-#if MXD_GITTELSON_PULSER_DEBUG
+#if MXD_GITTELSOHN_PULSER_DEBUG
 	MX_DEBUG(-2,("%s: pulser '%s', busy = %d",
 		fname, pulser->record->name, (int) pulser->busy));
 #endif
@@ -208,20 +208,20 @@ mxd_gittelson_pulser_is_busy( MX_PULSE_GENERATOR *pulser )
 }
 
 MX_EXPORT mx_status_type
-mxd_gittelson_pulser_start( MX_PULSE_GENERATOR *pulser )
+mxd_gittelsohn_pulser_start( MX_PULSE_GENERATOR *pulser )
 {
-	static const char fname[] = "mxd_gittelson_pulser_start()";
+	static const char fname[] = "mxd_gittelsohn_pulser_start()";
 
-	MX_GITTELSON_PULSER *gittelson_pulser = NULL;
+	MX_GITTELSOHN_PULSER *gittelsohn_pulser = NULL;
 	mx_status_type mx_status;
 
-	mx_status = mxd_gittelson_pulser_get_pointers( pulser,
-						&gittelson_pulser, fname );
+	mx_status = mxd_gittelsohn_pulser_get_pointers( pulser,
+						&gittelsohn_pulser, fname );
 
 	if ( mx_status.code != MXE_SUCCESS )
 		return mx_status;
 
-#if MXD_GITTELSON_PULSER_DEBUG
+#if MXD_GITTELSOHN_PULSER_DEBUG
 	MX_DEBUG(-2,("%s: Pulse generator '%s' starting, "
 		"pulse_width = %f, pulse_period = %f, num_pulses = %ld",
 			fname, pulser->record->name,
@@ -237,20 +237,20 @@ mxd_gittelson_pulser_start( MX_PULSE_GENERATOR *pulser )
 }
 
 MX_EXPORT mx_status_type
-mxd_gittelson_pulser_stop( MX_PULSE_GENERATOR *pulser )
+mxd_gittelsohn_pulser_stop( MX_PULSE_GENERATOR *pulser )
 {
-	static const char fname[] = "mxd_gittelson_pulser_stop()";
+	static const char fname[] = "mxd_gittelsohn_pulser_stop()";
 
-	MX_GITTELSON_PULSER *gittelson_pulser = NULL;
+	MX_GITTELSOHN_PULSER *gittelsohn_pulser = NULL;
 	mx_status_type mx_status;
 
-	mx_status = mxd_gittelson_pulser_get_pointers( pulser,
-						&gittelson_pulser, fname );
+	mx_status = mxd_gittelsohn_pulser_get_pointers( pulser,
+						&gittelsohn_pulser, fname );
 
 	if ( mx_status.code != MXE_SUCCESS )
 		return mx_status;
 
-#if MXD_GITTELSON_PULSER_DEBUG
+#if MXD_GITTELSOHN_PULSER_DEBUG
 	MX_DEBUG(-2,("%s: Stopping pulse generator '%s'.",
 		fname, pulser->record->name ));
 #endif
@@ -261,20 +261,20 @@ mxd_gittelson_pulser_stop( MX_PULSE_GENERATOR *pulser )
 }
 
 MX_EXPORT mx_status_type
-mxd_gittelson_pulser_get_parameter( MX_PULSE_GENERATOR *pulser )
+mxd_gittelsohn_pulser_get_parameter( MX_PULSE_GENERATOR *pulser )
 {
-	static const char fname[] = "mxd_gittelson_pulser_get_parameter()";
+	static const char fname[] = "mxd_gittelsohn_pulser_get_parameter()";
 
-	MX_GITTELSON_PULSER *gittelson_pulser = NULL;
+	MX_GITTELSOHN_PULSER *gittelsohn_pulser = NULL;
 	mx_status_type mx_status;
 
-	mx_status = mxd_gittelson_pulser_get_pointers( pulser,
-						&gittelson_pulser, fname );
+	mx_status = mxd_gittelsohn_pulser_get_pointers( pulser,
+						&gittelsohn_pulser, fname );
 
 	if ( mx_status.code != MXE_SUCCESS )
 		return mx_status;
 
-#if MXD_GITTELSON_PULSER_DEBUG
+#if MXD_GITTELSOHN_PULSER_DEBUG
 	MX_DEBUG(-2,
 	("%s invoked for PULSE_GENERATOR '%s', parameter type '%s' (%ld)",
 		fname, pulser->record->name,
@@ -306,7 +306,7 @@ mxd_gittelson_pulser_get_parameter( MX_PULSE_GENERATOR *pulser )
 		    mx_pulse_generator_default_get_parameter_handler( pulser );
 	}
 
-#if MXD_GITTELSON_PULSER_DEBUG
+#if MXD_GITTELSOHN_PULSER_DEBUG
 	MX_DEBUG(-2,("%s complete.", fname));
 #endif
 
@@ -314,20 +314,20 @@ mxd_gittelson_pulser_get_parameter( MX_PULSE_GENERATOR *pulser )
 }
 
 MX_EXPORT mx_status_type
-mxd_gittelson_pulser_set_parameter( MX_PULSE_GENERATOR *pulser )
+mxd_gittelsohn_pulser_set_parameter( MX_PULSE_GENERATOR *pulser )
 {
-	static const char fname[] = "mxd_gittelson_pulser_set_parameter()";
+	static const char fname[] = "mxd_gittelsohn_pulser_set_parameter()";
 
-	MX_GITTELSON_PULSER *gittelson_pulser = NULL;
+	MX_GITTELSOHN_PULSER *gittelsohn_pulser = NULL;
 	mx_status_type mx_status;
 
-	mx_status = mxd_gittelson_pulser_get_pointers( pulser,
-						&gittelson_pulser, fname );
+	mx_status = mxd_gittelsohn_pulser_get_pointers( pulser,
+						&gittelsohn_pulser, fname );
 
 	if ( mx_status.code != MXE_SUCCESS )
 		return mx_status;
 
-#if MXD_GITTELSON_PULSER_DEBUG
+#if MXD_GITTELSOHN_PULSER_DEBUG
 	MX_DEBUG(-2,
 	("%s invoked for PULSE_GENERATOR '%s', parameter type '%s' (%ld)",
 		fname, pulser->record->name,
---
+++ libMx/d_gittelsohn_pulser.h
@@ -0,0 +1,54 @@
+/*
+ * Name:    d_gittelsohn_pulser.h
+ *
+ * Purpose: Header file for Mark Gittelsohn's Arduino-based pulse generator.
+ *
+ * Author:  William Lavender
+ *
+ *--------------------------------------------------------------------------
+ *
+ * Copyright 2015 Illinois Institute of Technology
+ *
+ * See the file "LICENSE" for information on usage and redistribution
+ * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+ *
+ */
+
+#ifndef __D_GITTELSOHN_PULSER_H__
+#define __D_GITTELSOHN_PULSER_H__
+
+#include "mx_pulse_generator.h"
+
+typedef struct {
+	MX_RECORD *record;
+
+	MX_RECORD *rs232_record;
+} MX_GITTELSOHN_PULSER;
+
+/* Define all of the interface functions. */
+
+MX_API mx_status_type mxd_gittelsohn_pulser_create_record_structures(
+							MX_RECORD *record );
+MX_API mx_status_type mxd_gittelsohn_pulser_open( MX_RECORD *record );
+
+MX_API mx_status_type mxd_gittelsohn_pulser_is_busy( MX_PULSE_GENERATOR *pulser );
+MX_API mx_status_type mxd_gittelsohn_pulser_start( MX_PULSE_GENERATOR *pulser );
+MX_API mx_status_type mxd_gittelsohn_pulser_stop( MX_PULSE_GENERATOR *pulser );
+MX_API mx_status_type mxd_gittelsohn_pulser_get_parameter(
+					MX_PULSE_GENERATOR *pulser );
+MX_API mx_status_type mxd_gittelsohn_pulser_set_parameter(
+					MX_PULSE_GENERATOR *pulser );
+
+extern MX_RECORD_FUNCTION_LIST mxd_gittelsohn_pulser_record_function_list;
+extern MX_PULSE_GENERATOR_FUNCTION_LIST mxd_gittelsohn_pulser_pulser_function_list;
+
+extern long mxd_gittelsohn_pulser_num_record_fields;
+extern MX_RECORD_FIELD_DEFAULTS *mxd_gittelsohn_pulser_rfield_def_ptr;
+
+#define MXD_GITTELSOHN_PULSER_STANDARD_FIELDS \
+  {-1, -1, "rs232_record", MXFT_RECORD, NULL, 0, {0}, \
+	MXF_REC_TYPE_STRUCT, offsetof(MX_GITTELSOHN_PULSER, rs232_record), \
+	{0}, NULL, (MXFF_IN_DESCRIPTION | MXFF_IN_SUMMARY) }, \
+
+#endif /* __D_GITTELSOHN_PULSER_H__ */
+
