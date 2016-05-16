70bdca7af5b671ebcad73f34faa0a312d58e9e76
lavender <lavender@42547f41-83fc-0310-9aef-9668e019ab9e>
2016-05-16 17:02:17 +0000

    Hurd fixes
    git-svn-id: http://svn.csrri.iit.edu/mx/trunk@3481 42547f41-83fc-0310-9aef-9668e019ab9e

--- libMx/mx_net_interface.c
+++ libMx/mx_net_interface.c
@@ -331,7 +331,8 @@ mx_network_get_interface_from_host_address( MX_NETWORK_INTERFACE **ni,
 #elif ( defined( OS_LINUX ) && ( MX_GLIBC_VERSION >= 2003000L ) ) \
 	|| ( defined( OS_LINUX ) && defined( MX_MUSL_VERSION ) ) \
 	|| ( defined( OS_SOLARIS ) && ( MX_SOLARIS_VERSION >= 5011000L ) ) \
-	|| defined( OS_BSD ) || defined( OS_CYGWIN ) || defined( OS_RTEMS )
+	|| defined( OS_BSD ) || defined( OS_CYGWIN ) || defined( OS_RTEMS ) \
+	|| defined( OS_HURD )
 
 #include <stdio.h>
 #include <stdlib.h>
