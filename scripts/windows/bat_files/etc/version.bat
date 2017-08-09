7f5e81c9e958e34dcf88fc7f430f6cddd58a0cc5
lavender <lavender@42547f41-83fc-0310-9aef-9668e019ab9e>
2017-08-09 19:08:05 +0000

    Update mx/scripts/windows/bat_files/etc/version.bat
    git-svn-id: http://svn.csrri.iit.edu/mx/trunk@3823 42547f41-83fc-0310-9aef-9668e019ab9e

--- scripts/windows/bat_files/etc/version.bat
+++ scripts/windows/bat_files/etc/version.bat
@@ -1,17 +1,15 @@
 @echo off
 
-set MX_VERSION=mx-2.1.1-2017-06-05
+set MX_VERSION=mx-2.1.1-2017-08-07
 
-set MX_PYTHON_VERSION=27
+set MX_PYTHON_DIR=c:\WinPython-64bit-2.7.10.2\python-2.7.10.amd64
 
 rem =======================================================================
 
 set MXDIR=c:/opt/%MX_VERSION%
 set MXWDIR=c:\opt\%MX_VERSION%
 
-set EPICSBASE=c:\opt\epics\base-%EPICS_VERSION%
-
-set PATH=c:\opt\mx\bin;%MXWDIR%\bin;%MXWDIR%\lib\modules;c:\python%MX_PYTHON_VERSION%;c:\python%MX_PYTHON_VERSION%\scripts;%PATH%;c:\cygwin\bin
+set PATH=c:\opt\local\bin;c:\opt\mx\bin;%MXWDIR%\bin;%MXWDIR%\lib\modules;%MX_PYTHON_DIR%;%MX_PYTHON_DIR%\scripts;%PATH%;c:\cygwin64\bin;c:\opt\tiff-4.0.6\libtiff;c:\Program Files (x86)\Sysinternals
 
 set PYTHONPATH=%MXWDIR%\lib\mp;%PYTHONPATH%
 
