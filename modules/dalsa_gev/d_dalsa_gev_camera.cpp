1e18da537cae103da31ec1315c911e0342809652
lavender <lavender@42547f41-83fc-0310-9aef-9668e019ab9e>
2017-10-23 23:48:48 +0000

    Some work on the 'dalsa_gev' module for Dalsa GigE Vision cameras.
    git-svn-id: http://svn.csrri.iit.edu/mx/trunk@3882 42547f41-83fc-0310-9aef-9668e019ab9e

--- modules/dalsa_gev/d_dalsa_gev_camera.cpp
+++ modules/dalsa_gev/d_dalsa_gev_camera.cpp
@@ -8,7 +8,7 @@
  *
  *--------------------------------------------------------------------------
  *
- * Copyright 2016 Illinois Institute of Technology
+ * Copyright 2016-2017 Illinois Institute of Technology
  *
  * See the file "LICENSE" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
