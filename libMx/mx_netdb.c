7505e0e9af0751507a077a3296e5c443c3bdf7d3
lavender <lavender@42547f41-83fc-0310-9aef-9668e019ab9e>
2018-12-21 01:05:06 +0000

    libMx/mx_netdb.c contains the beginnings of implementations of getaddrinfo() and getnameinfo() for systems that are too old to have them.
    git-svn-id: http://svn.csrri.iit.edu/mx/trunk@4167 42547f41-83fc-0310-9aef-9668e019ab9e

---
+++ libMx/mx_netdb.c
@@ -0,0 +1,135 @@
+/*
+ * Name:    mx_netdb.c
+ *
+ * Purpose: Implements getaddrinfo() and getnameinfo() using the older
+ *          gethostbyname() and gethostbyaddr() functions.
+ *
+ * Author:  William Lavender
+ *
+ * Some old build targets do not come with native versions of getaddrinfo()
+ * and getnameinfo().  This interface provides versions of those function
+ * built on top of the old, deprecated gethostbyname() and gethostbyaddr()
+ * functions.  Of course, these versions only provide support for IPV4.
+ *
+ * WARNING: The functions in this file are not yet tested!!!  Do not
+ *          use them just yet.
+ *
+ *----------------------------------------------------------------------
+ *
+ * Copyright 2018 Illinois Institute of Technology
+ *
+ * See the file "LICENSE" for information on usage and redistribution
+ * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#include "mx_util.h"
+#include "mx_netdb.h"
+#include "mx_socket.h"
+
+MX_EXPORT int
+mx_getaddrinfo( const char *node,
+		const char *service,
+		const struct addrinfo *hints,
+		struct addrinfo **res )
+{
+	struct hostent *hostent_ptr = NULL;
+	struct servent *servent_ptr = NULL;
+
+	struct addrinfo *new_addrinfo = NULL;
+	struct sockaddr_in *new_sockaddr_in = NULL;
+
+	if ( node == (const char *) NULL ) {
+		errno = EINVAL;
+		return EAI_SYSTEM;
+	}
+
+	/* Allocate memory for 'new_addrinfo' before trying to do the lookup. */
+
+	new_addrinfo = calloc( 1, sizeof(struct addrinfo) );
+
+	if ( new_addrinfo == (struct addrinfo *) NULL ) {
+		errno = ENOMEM;
+		return EAI_SYSTEM;
+	}
+
+	new_sockaddr_in = calloc( 1, sizeof(struct sockaddr_in) );
+
+	if ( new_sockaddr_in == (struct sockaddr_in *) NULL ) {
+		errno = ENOMEM;
+		return EAI_SYSTEM;
+	}
+
+
+	/*---*/
+
+	hostent_ptr = gethostbyname( node );
+
+	if ( hostent_ptr == (struct hostent *) NULL ) {
+		errno = EINVAL;
+		return EAI_SYSTEM;
+	}
+
+	servent_ptr = getservbyname( service, NULL );
+
+	if ( servent_ptr == (struct servent *) NULL ) {
+		errno = EINVAL;
+		return EAI_SYSTEM;
+	}
+
+	new_addrinfo->ai_flags = 0x0;
+	new_addrinfo->ai_family = AF_INET;
+	new_addrinfo->ai_socktype = SOCK_STREAM;
+	new_addrinfo->ai_protocol = AF_INET;
+	new_addrinfo->ai_addrlen = sizeof(struct sockaddr_in);
+
+	new_sockaddr_in->sin_family = AF_INET;
+	new_sockaddr_in->sin_addr.s_addr = hostent_ptr->h_addr[0];
+	new_sockaddr_in->sin_port = servent_ptr->s_port;
+
+	new_addrinfo->ai_addr = (struct sockaddr *) new_sockaddr_in;
+
+	new_addrinfo->ai_canonname = strdup( hostent_ptr->h_name );
+	new_addrinfo->ai_next = NULL;
+
+	*res = new_addrinfo;
+
+	return 0;
+}
+
+MX_EXPORT void
+mx_freeaddrinfo( struct addrinfo *res )
+{
+	if ( res == (struct addrinfo *) NULL ) {
+		return;
+	}
+
+	mx_free( res->ai_addr );
+	mx_free( res->ai_canonname );
+	mx_free( res );
+
+	return;
+}
+
+MX_EXPORT const char *
+mx_gai_strerror( int foo )
+{
+	return NULL;
+}
+
+MX_EXPORT int
+mx_getnameinfo( const struct sockaddr *sa,
+		socklen_t salen,
+		char *host,
+		socklen_t hostlen,
+		char *serv,
+		socklen_t servlen,
+		int flags )
+{
+	return (-1);
+}
+
---
+++ libMx/mx_netdb.h
@@ -0,0 +1,105 @@
+/*
+ * Name:    mx_netdb.h 
+ *
+ * Purpose: Implements getaddrinfo() and getnameinfo() using the older
+ *          gethostbyname() and gethostbyaddr() functions.
+ *
+ * Author:  William Lavender
+ *
+ * Some old build targets do not come with native versions of getaddrinfo()
+ * and getnameinfo().  This interface provides versions of those function
+ * built on top of the old, deprecated gethostbyname() and gethostbyaddr()
+ * functions.  Of course, these versions only provide support for IPV4.
+ *
+ *----------------------------------------------------------------------
+ *
+ * Copyright 2018 Illinois Institute of Technology
+ *
+ * See the file "LICENSE" for information on usage and redistribution
+ * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+ *
+ */
+
+#ifndef __MX_NETDB_H__
+#define __MX_NETDB_H__
+
+#include <netdb.h>
+
+#if !defined( PF_UNSPEC )
+#  define PF_UNSPEC	0
+#endif
+
+#if !defined( PF_INET6 )
+#  define PF_INET6	10
+#endif
+
+#if !defined( AF_UNSPEC )
+#  define AF_UNSPEC	0
+#endif
+
+#if !defined( AF_INET6 )
+#  define AF_INET6	10
+#endif
+
+#if !defined( _NETDB_H )
+
+  struct addrinfo {
+    int              ai_flags;
+    int              ai_family;
+    int              ai_socktype;
+    int              ai_protocol;
+    socklen_t        ai_addrlen;
+    struct sockaddr *ai_addr;
+    char            *ai_canonname;
+    struct addrinfo *ai_next;
+  }
+
+#endif
+
+/*----*/
+
+MX_API int
+mx_getaddrinfo( const char *node,
+		const char *service,
+		const struct addrinfo *hints,
+		struct addrinfo **res );
+
+MX_API void
+mx_freeaddrinfo( struct addrinfo *res );
+
+MX_API const char *
+mx_gai_strerror( int errcode );
+
+/*----*/
+
+/* flags argument for mx_getnameinfo(). */
+
+#if !defined( NI_NAMEREQD )
+#  define NI_NUMERICHOST 0x1
+#  define NI_NUMERICSERV 0x2
+#  define NI_NOFQDN      0x4
+#  define NI_NAMEREQD    0x8
+#  define NI_DGRAM       0x10
+#endif
+
+MX_API int
+mx_getnameinfo( const struct sockaddr *sa,
+		socklen_t salen,
+		char *host,
+		socklen_t hostlen,
+		char *serv,
+		socklen_t servlen,
+		int flags );
+
+#if 0
+#  define getaddrinfo( n, s, h, r )	mx_getaddrinfo( n, s, h, r )
+
+#  define getnameinfo( s, sl, h, hl, r, rl, f ) \
+		mx_getnameinfo( s, sl, h, hl, r, rl, f )
+
+#  define freeaddrinfo( r )	mx_freeaddrinfo( r )
+
+#  define gai_strerror( e )	mx_gai_strerror( e )
+#endif
+
+#endif /* __MX_NETDB_H__ */
