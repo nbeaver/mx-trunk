bcb6ca52f51154f658b15816a97d35c76308dac9
lavender <lavender@42547f41-83fc-0310-9aef-9668e019ab9e>
2019-07-09 20:24:50 +0000

    Deleted old 'fastcam_pcclib' module.
    git-svn-id: http://svn.csrri.iit.edu/mx/trunk@4373 42547f41-83fc-0310-9aef-9668e019ab9e

--- modules/fastcam_pcclib/test/mfastcam_pcclib.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-set path=..\..\..\libMx;%path%
-
-..\..\..\motor\motor -F mfastcam_pcclib.dat -s sfastcam_pcclib.dat %1 %2 %3 %4 %5 %6 %7 %8 %9
-
--- modules/fastcam_pcclib/test/setwin32.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-set path=%path%;c:\docume~1\lavender\archives\photron\PFV2452\SDK\VC\Dll
-
