cdf6c29643a8ae19f5beeaa16be76b82d587911d
lavender <lavender@42547f41-83fc-0310-9aef-9668e019ab9e>
2020-07-07 00:46:47 +0000

    Some initial updates for DANTE 3.4.0.3
    git-svn-id: http://svn.csrri.iit.edu/mx/trunk@4573 42547f41-83fc-0310-9aef-9668e019ab9e

--- modules/xglab_dante/test/mdante.bat
+++ modules/xglab_dante/test/mdante.bat
@@ -1,6 +1,7 @@
 @echo off
 
-set DANTE_DLL_DIR=c:\Users\mrcat\Desktop\DANTE\Library\Microsoft Visual C++ 2017 x64\2019-06-24 17.07-3.2.4.3-Callback-x64\Release
+set DANTE_DLL_DIR=c:\opt\DANTE\3.2.4.3\Library\Callback-x64\Release
+rem set DANTE_DLL_DIR=c:\opt\DANTE\3.4.0.3\Library\Callback-x64\Release
 
 set path=..;%DANTE_DLL_DIR%;%path%
 
