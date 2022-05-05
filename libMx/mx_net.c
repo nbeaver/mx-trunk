14509838879a7986fc59f1de2fc40c0dd5f2efa5
lavender <lavender@42547f41-83fc-0310-9aef-9668e019ab9e>
2022-05-05 21:58:40 +0000

    Bugfix for calculation of mx_num_elements in the new infrastructure for mx_put_field_array()
    git-svn-id: http://svn.csrri.iit.edu/mx/trunk@5041 42547f41-83fc-0310-9aef-9668e019ab9e

--- libMx/mx_net.c
+++ libMx/mx_net.c
@@ -30,7 +30,7 @@
 
 #define NETWORK_DEBUG_NEW_COPY_GET_FIELD_ARRAY	FALSE
 
-#define NETWORK_DEBUG_NEW_COPY_PUT_FIELD_ARRAY	TRUE
+#define NETWORK_DEBUG_NEW_COPY_PUT_FIELD_ARRAY	FALSE
 
 #include <stdio.h>
 #include <string.h>
@@ -6012,14 +6012,11 @@ mx_new_copy_put_field_array( MX_RECORD *server_record,
 			fname, server->record->name,
 			server->use_64bit_network_longs));
 #endif
-#if 0
-		mx_breakpoint();
-#endif
 
 		mx_num_elements = 1;
 
 		for ( i = 0; i < local_field->num_dimensions; i++ ) {
-			mx_num_elements += local_field->dimension[i];
+			mx_num_elements *= local_field->dimension[i];
 		}
 
 #if NETWORK_DEBUG_NEW_COPY_PUT_FIELD_ARRAY
@@ -6717,8 +6714,6 @@ mx_put_field_array( MX_RECORD *server_record,
 					server->use_64bit_network_longs );
 		fprintf( stderr, ")\n" );
 	}
-
-	message_length += header_length;
 
 #if NETWORK_DEBUG_TIMING
 	MX_HRT_START( measurement );
