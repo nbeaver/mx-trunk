986647a073d23a4e01271a9d6b7292af0743fa3e
lavender <lavender@42547f41-83fc-0310-9aef-9668e019ab9e>
2022-05-30 20:59:03 +0000

    Added num_elements field to MX_RECORD_FIELD
    git-svn-id: http://svn.csrri.iit.edu/mx/trunk@5078 42547f41-83fc-0310-9aef-9668e019ab9e

--- libMx/mx_record.h
+++ libMx/mx_record.h
@@ -104,6 +104,7 @@ typedef struct mx_record_field_type {
 	void *typeinfo;
 	long num_dimensions;
 	long *dimension;
+	unsigned long num_elements;
 	size_t *data_element_size;
 	size_t max_bytes;
 	void *data_pointer;
